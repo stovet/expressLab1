0ca52c1a747e8e264dcab05c84b6e1289d0d861f
stovet <steven.topalian@gmail.com>
2021-12-30 16:21:56 -0500

    put and post

--- src/routes/cart-items.ts
+++ src/routes/cart-items.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import Cart from '../models/cart'
 const cartRoutes = express.Router();
 
-const cart: Cart[] = [
+const carts: Cart[] = [
     {id: 1, product: "coffee", price: 8, quantity: 1},
     {id: 2, product: "pizza", price: 5, quantity: 2},
     {id: 3, product: "Fancy eggs", price: 2, quantity: 1},
     {id: 4, product: "juice", price: 1, quantity: 3}
 ];
+let nextId: number = 5;
 
 cartRoutes.get("/", function(req, res){
     let maxPriceParam: string = req.query.maxPrice as string;
@@ -15,13 +16,13 @@ cartRoutes.get("/", function(req, res){
     let pageSizeParam: string = req.query.pageSize as string;
     if(maxPriceParam){
         let maxPrice: number = parseFloat(maxPriceParam);
-        let filteredCart: Cart[] = cart.filter(item => {
+        let filteredCart: Cart[] = carts.filter(item => {
             return item.price <= maxPrice;
         });
         res.json(filteredCart);
     } else if(fancyParam){
         let fancy: string = fancyParam;
-        let fancyCart: Cart[] = cart.filter(item => {
+        let fancyCart: Cart[] = carts.filter(item => {
             if(item.product.includes("Fancy", 0)){
                return item;
             };
@@ -30,23 +31,23 @@ cartRoutes.get("/", function(req, res){
     } else if(pageSizeParam){
         let pageSize: number = parseInt(pageSizeParam);
         let maxCart: Cart[] = [];
-        for(let i = 0; i < pageSize && i < cart.length; i++){
-            maxCart.push(cart[i]);
+        for(let i = 0; i < pageSize && i < carts.length; i++){
+            maxCart.push(carts[i]);
         }
         res.json(maxCart);
     }
     
     else {
     res.status(200);
-    res.json(cart);
+    res.json(carts);
     }    
 });
 
 cartRoutes.get("/:id", function(req, res){
     let inputId: number = parseInt(req.params.id);
-    for(let i = 0; i < cart.length; i++){
-        if(cart[i].id === inputId){
-            res.json(cart[i]);
+    for(let i = 0; i < carts.length; i++){
+        if(carts[i].id === inputId){
+            res.json(carts[i]);
             res.status(200);
             break;
         }
@@ -55,5 +56,35 @@ cartRoutes.get("/:id", function(req, res){
     res.send({"error": "ID Not Found"})
 });
 
+cartRoutes.post("/", function(req, res){
+    let newCart: Cart = req.body;
+    newCart.id = nextId;
+    nextId += 1;
+    carts.push(newCart);
+    res.status(201);
+    res.json(carts);
+});
+
+cartRoutes.put("/:id", function(req, res){
+    let inputId: number = parseInt(req.params.id);
+    let newCart: Cart = req.body;
+    newCart.id = inputId;
+    for(let i = 0; i < carts.length; i++){
+        if(carts[i].id === inputId){
+            carts.splice(i, 1, newCart)
+        }
+    }
+    res.status(200);
+    res.json(carts);
+});
+
+
+cartRoutes.delete("/:id", function(req, res){
+    let inputId: number = parseInt(req.params.id);
+    let cartIndex = carts.findIndex(cart => cart.id === inputId);
+    carts.splice(cartIndex, 1);
+    res.status(404);
+    res.json("");
+});
 
 export default cartRoutes;
